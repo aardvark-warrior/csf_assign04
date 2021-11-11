72c69bc67392e3234f401f1965d09a7d8ade317d
Mason Albert <malber20@jhu.edu>
2021-11-11 11:49:49 -0500

    began work on magic.cpp, making sys calls

--- magic.cpp
+++ magic.cpp
@@ -15,4 +15,30 @@
 
 int main(int argc, char **argv) {
   // TODO: implement
+  int fd = open(argv[1], O_RDONLY);
+
+  if (fd < 0) {
+    // error - do something
+    return 1;
+  }
+
+struct stat statbuf;
+int rc = fstat(fd, &statbuf);
+if (rc != 0) {
+  // error - do something
+  return 1;
+}
+size_t file_size = statbuf.st_size;
+// ...
+void *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+if (data == (void*) -1) {
+  // error - do something
+  return 1;
+}
+
+Elf64_Ehdr *elf_header = (Elf64_Ehdr *) data;
+
+
+
+
 }
